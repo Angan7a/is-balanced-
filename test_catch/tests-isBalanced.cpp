6c274a226f112a0815fdf51f82ab61c19fc5a656
Andrzej <ganczarek.a@gmil.com>
2018-08-13 19:57:51 +0200

    Added some tests

--- test_catch/tests-isBalanced.cpp
+++ test_catch/tests-isBalanced.cpp
@@ -82,4 +82,24 @@ SCENARIO("Is this string balanced?") {
             }
         }
     }
+    GIVEN("string }}}}}}}}}}]{}]") {
+        std::string s = {"}}}}}}}}}}]{}]"};
+        WHEN("call function isBalanced()")
+        {
+            THEN("Expect 0")
+            {
+                REQUIRE(isBalanced(s) == 0);
+            }
+        }
+    }
+    GIVEN("string {}{{}}") {
+        std::string s = {"{}{{}}"};
+        WHEN("call function isBalanced()")
+        {
+            THEN("Expect 1")
+            {
+                REQUIRE(isBalanced(s) == 1);
+            }
+        }
+    }
 }
