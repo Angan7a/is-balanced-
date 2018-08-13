3911a6b38cee9b0d331ae5a6ef88ac9161ddfb4b
Andrzej <ganczarek.a@gmail.com>
2018-08-13 13:58:28 +0200

    Given ({)}

--- src/isBalanced.cpp
+++ src/isBalanced.cpp
@@ -5,29 +5,29 @@ bool isBalanced(std::string s)
 {
     if (s.size() % 2 == 0 && s.size() != 0)
     {
-        std::vector<int> a, b, c;
+        std::vector<char> a;
         for (int i = 0; i < s.size(); i++)
         {
-            if (s[i] == '{') a.push_back(1);
-            if (s[i] == '[') b.push_back(1);
-            if (s[i] == '(') c.push_back(1);
+            if (s[i] == '{') a.push_back('{');
+            if (s[i] == '[') a.push_back('[');
+            if (s[i] == '(') a.push_back('(');
             if (s[i] == '}')
             {
-                if (a.empty()) return 0;
+                if (a.empty() || a.back() != '{') return 0;
                 a.pop_back();
             }
             if (s[i] == ']')
             {
-                if (b.empty()) return 0;
-                b.pop_back();
+                if (a.empty() || a.back() != '[') return 0;
+                a.pop_back();
             }
             if (s[i] == ')')
             {
-                if (c.empty()) return 0;
-                c.pop_back();
+                if (a.empty() || a.back() != '(') return 0;
+                a.pop_back();
             }
         }
-        if (a.empty() && b.empty() && c.empty()) return 1;
+        if (a.empty()) return 1;
     }
     return 0;
 }
