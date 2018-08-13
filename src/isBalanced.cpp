92feb6918bfdc330f2ac3e19bd57bc59c5891d99
Andrzej <ganczarek.a@gmil.com>
2018-08-13 18:52:00 +0200

    Given {{}{

--- src/isBalanced.cpp
+++ src/isBalanced.cpp
@@ -2,6 +2,11 @@
 
 bool isBalanced(std::string s)
 {
-    if (s.size()%2 == 0) return 1;
+    if (s.size()%2 == 0)
+    {
+        auto iter = s.find("{}");
+        s.erase(iter, 2);
+        if (s.empty()) return 1;
+    }
     return 0;
 }
--- test_catch/tests-isBalanced.cpp
+++ test_catch/tests-isBalanced.cpp
@@ -13,13 +13,22 @@ SCENARIO("Is this string balanced?") {
         }
     }
     GIVEN("string {}") {
-        std::string s = {"{"};
+        std::string s = {"{}"};
         WHEN("call function isBalanced()")
         {
-            s = "{}";
             THEN("Expect 1")
             {
                 REQUIRE(isBalanced(s) == 1);
+            }
+        }
+    }
+    GIVEN("string {{}{") {
+        std::string s = {"{{}{"};
+        WHEN("call function isBalanced()")
+        {
+            THEN("Expect 0")
+            {
+                REQUIRE(isBalanced(s) == 0);
             }
         }
     }
