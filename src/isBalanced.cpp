89ed47167b170b10ff17c78c7358564fa820cb00
Andrzej <ganczarek.a@gmil.com>
2018-08-13 18:41:05 +0200

    Given {

--- src/isBalanced.cpp
+++ src/isBalanced.cpp
@@ -1,30 +1,6 @@
 #include "../inc/isBalanced.hpp"
-#include <vector>
 
 bool isBalanced(std::string s)
 {
-    if (s.size() % 2 == 0 && s.size() != 0)
-    {
-        std::vector<char> a;
-        for (int i = 0; i < s.size(); i++)
-        {
-            switch (s[i])
-            {
-                case '{' : a.push_back('{'); break;
-                case '[' : a.push_back('['); break;
-                case '(' : a.push_back('('); break;
-                case '}' :
-                    if (a.empty() || a.back() != '{') return 0;
-                    a.pop_back(); break;
-                case ']' :
-                    if (a.empty() || a.back() != '[') return 0;
-                    a.pop_back(); break;
-                case ')' :
-                    if (a.empty() || a.back() != '(') return 0;
-                    a.pop_back(); break;
-            }
-        }
-        if (a.empty()) return 1;
-    }
     return 0;
 }
