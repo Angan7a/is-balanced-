deb26e431b2bef2f33c1c1fa5c15bef710d299f7
Andrzej <ganczarek.a@gmil.com>
2018-08-13 19:38:09 +0200

    Refactoring

--- src/isBalanced.cpp
+++ src/isBalanced.cpp
@@ -1,16 +1,16 @@
 #include "../inc/isBalanced.hpp"
-
+#include <iostream>
 bool isBalanced(std::string s)
 {
     if (s.size()%2 == 0)
     {
-        for(int i = 0; i <= s.size()/2; i++)
+        while(!s.empty())
         {
             auto iter = s.find("{}");
             if (iter == std::string::npos) return 0;
             s.erase(iter, 2);
         }
-        if (s.empty()) return 1;
+        return 1;
     }
     return 0;
 }
