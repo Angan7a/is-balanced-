91f058b3883363b669510a291a7ef7f9852ab51e
Andrzej <ganczarek.a@gmail.com>
2018-08-13 12:33:42 +0200

    Given {{{}

--- src/isBalanced.cpp
+++ src/isBalanced.cpp
@@ -4,11 +4,12 @@ bool isBalanced(std::string s)
 {
     if (s.size() % 2 == 0)
     {
+        int r = 0;
         for (int i = 0; i < s.size()/2; i++)
         {
-            if (s[i] == '{' && s[s.size()-1-i] == '}');
+            if (s[i] == '{' && s[s.size()-1-i] == '}') r++;
         }
-        return 1;
+        if (r == s.size()/2) return 1;
     }
     return 0;
 }
